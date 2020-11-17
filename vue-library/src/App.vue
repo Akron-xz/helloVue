e2a255b3d6931f9bac83b2bf953b98004abb6d8b
Akron-xz <502793920@qq.com>
2020-11-17 21:31:12 +0800

    xz

--- vue-library/src/App.vue
+++ vue-library/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav" v-show="false">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
--- vue-library/src/views/Home.vue
+++ vue-library/src/views/Home.vue
@@ -1,17 +1,79 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <div class="home-container">
+        <div class="admin" @click="toAdmin">
+            管理员
+        </div>
+
+        <div class="login" @click="toLogin">
+            登陆
+        </div>
+
+        <div class="signup" @click="toSignUp">
+            注册
+        </div>
+    </div>
+</template>>
+
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    methods: {
+        toLogin(){
+            this.$router.push({path:'/UserLogin'})
+        },
+        toAdmin(){
+            this.$router.push({path:'/admin'})
+        },
+        toSignUp(){
+            this.$router.push({path:'/signup'})
+        },
+    },
 }
 </script>
+
+<style  scoped>
+.home-container{
+    position: fixed;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+    background-size: 100%;
+    background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+    text-align: center;
+}
+
+.admin{
+    position: absolute;
+    left:50%;
+    width: 300px;
+    height: 100px;
+    margin-left: -150px;
+    margin-top: 50px;
+    background-color: red;
+    cursor: pointer;
+}
+
+.login{
+    position: absolute;
+    left:25%;
+    width: 300px;
+    height: 100px;
+    margin-left: -150px;
+    margin-top: 250px;
+    background-color: blue;
+    cursor: pointer;
+}
+
+.signup{
+    position: absolute;
+    right:25%;
+    width: 300px;
+    height: 100px;
+    margin-right: -150px;
+    margin-top: 250px;
+    background-color: black;
+cursor: pointer;
+}
+
+</style>>
