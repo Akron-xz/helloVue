3fd562eb7f648f69b0d64179197fbee11034bcc3
Akron-xz <502793920@qq.com>
2020-11-19 19:43:25 +0800

    xz

--- vue-library/src/views/Home.vue
+++ vue-library/src/views/Home.vue
@@ -1,103 +1,128 @@
 <template>
-    <div class="home-container">
-
-         <div class="login box">
-            <img src="/img/home/login.jpg" alt="" class="home-img" @click="toLogin">
-            <p class="logo-name" @click="toLogin">登陆</p>
-        </div>
-
-        <div class="admin box" >
-            <img src="/img/home/manage.jpg" alt="" class="home-img" @click="toAdmin">
-            <p class="logo-name" @click="toAdmin">图书管理</p>
-        </div>
-
-        <div class="signup box">
-            <img src="/img/home/signup.jpg" alt="" class="home-img" @click="toSignUp">
-            <p class="logo-name" @click="toSignUp">注册</p>
-        </div>
-
-        <div class="announce-box" >
-            <p>{{announceContent}}</p>
-        </div>
+  <div class="home-container">
+    <div class="login box">
+      <img src="/img/home/login.jpg" alt="" class="home-img" @click="toLogin" />
+      <p class="logo-name" @click="toLogin">登陆</p>
     </div>
+
+    <div class="admin box">
+      <img
+        src="/img/home/manage.jpg"
+        alt=""
+        class="home-img"
+        @click="toAdmin"
+      />
+      <p class="logo-name" @click="toAdmin">图书管理</p>
+    </div>
+
+    <div class="signup box">
+      <img
+        src="/img/home/signup.jpg"
+        alt=""
+        class="home-img"
+        @click="toSignUp"
+      />
+      <p class="logo-name" @click="toSignUp">注册</p>
+    </div>
+
+    <div class="announce-box" >
+      <p></p>
+    </div>
+  </div>
 </template>>
 
 
 <script>
-
+import axios from "axios";
 export default {
-   data() {
-        return {
-            announceContent:"测试公告",
-            announceTime:""
-        }
+  data() {
+    return {
+     
+      announceTime: "",
+      announce:[ ]
+     
+
+      
+    };
+  },
+  methods: {
+    getAnnounce() {
+      axios({
+              method:"get",
+              url:"data/announce.json"
+            }).then(res=>{
+                let list = res.data;
+              this.announce = list; 
+                    
+              
+            })
+        .catch(function (error) {
+          //请求失败
+          console.log("error...", error);
+        });
     },
-    methods: {
-        toLogin(){
-            this.$router.push({path:'/UserLogin'})
-        },
-        toAdmin(){
-            this.$router.push({path:'/admin'})
-        },
-        toSignUp(){
-            this.$router.push({path:'/signup'})
-        },
+    toLogin() {
+      this.$router.push({ path: "/UserLogin" });
     },
-    created() {
-        
+    toAdmin() {
+      this.$router.push({ path: "/admin" });
     },
-}
+    toSignUp() {
+      this.$router.push({ path: "/signup" });
+    },
+  },
+  created() {
+      this.getAnnounce();
+  },
+};
 </script>
 
 <style  scoped>
-.home-container{
-    position: fixed;
-    left: 0;
-    top:0;
-    width: 100%;
-    height: 100%;
-    background-repeat: no-repeat;
-    background-size: 100%;
-    background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
-    text-align: center;
+.home-container {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+  text-align: center;
 }
 
-.home-img{
-    width: 150px;
-    height:150px;
-    margin-top: 50px;
-    border-radius: 80px;
-    cursor: pointer;
-    transition: width 1s, height 1s, transform 1s;
+.home-img {
+  width: 150px;
+  height: 150px;
+  margin-top: 50px;
+  border-radius: 80px;
+  cursor: pointer;
+  transition: width 1s, height 1s, transform 1s;
 }
 
-.home-img:hover{
-    width: 180px;
-    height:180px;
+.home-img:hover {
+  width: 180px;
+  height: 180px;
 }
 
-.logo-name:hover{
-    cursor: pointer;
-    color: blue;
+.logo-name:hover {
+  cursor: pointer;
+  color: blue;
 }
 
-.box{
-    list-style-type: none;
-    float: left;
-    margin: auto;
-    width: 33%;
+.box {
+  list-style-type: none;
+  float: left;
+  margin: auto;
+  width: 33%;
 }
-.announce-box{
-    position: fixed;
-    bottom: 70px;
-    left: 50%;
-    height: 200px;
-    width: 80%;
-    margin-left: -40%;
-    background-color: #ffffff70;
-    border: 2px solid black;
-
+.announce-box {
+  position: fixed;
+  bottom: 70px;
+  left: 50%;
+  height: 200px;
+  width: 80%;
+  margin-left: -40%;
+  background-color: #ffffff70;
+  border: 2px solid black;
 }
-
-
 </style>>
