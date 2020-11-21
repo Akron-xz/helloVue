b5b39a98d812ab29f26402947b8ffe7ecfa00268
test <fangdeng12@gmail.com>
2020-11-21 12:22:13 +0800

    xz

--- vue-library/src/views/ModifyMsg.vue
+++ vue-library/src/views/ModifyMsg.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="table-container">
-    
     <table
       border="1"
       cellspacing="0"
@@ -16,10 +15,9 @@
         <td prop="Name">
           <input
             type="text"
-            v-model="Name" 
+            v-model="Name"
             maxlength="20"
             style="width: 190px; height: 30px"
-            
           />
         </td>
         <td class="msg-header">性别：</td>
@@ -46,7 +44,7 @@
         <td>
           <input
             type="text"
-         v-model="Email"
+            v-model="Email"
             maxlength="30"
             style="width: 190px; height: 30px"
           />
@@ -93,11 +91,10 @@
         </td>
       </tr>
     </table>
-    
+
     <br /><br />
 
     <div>
-      
       <el-button type="primary" class="modify-btn">编辑</el-button>
     </div>
     <div class="modify-pasawd-box">
@@ -127,14 +124,14 @@
 export default {
   data() {
     return {
-      Name: '',
-      Sex:'',
-      Age:'',
-      Email:'',
-      Birthday:'',
-      Phone:'',
-      Adress:'',
-      Introduction:'',
+      Name: "",
+      Sex: "",
+      Age: "",
+      Email: "",
+      Birthday: "",
+      Phone: "",
+      Adress: "",
+      Introduction: "",
       ruleForm: {
         password: "",
       },
@@ -142,9 +139,6 @@ export default {
         password: [{ required: true, message: "请输入密码", trigger: "blur" }],
         password2: [{ required: true, message: "请输入密码", trigger: "blur" }],
       },
-      
-            
-         
     };
   },
   methods: {
@@ -183,10 +177,10 @@ export default {
   right: 370px;
 
   top: 80px;
-  border-color: rgb(231, 231, 231) ;
+  border-color: rgb(231, 231, 231);
   background: white;
 }
-.tableid input{
+.tableid input {
   border: 0;
   background: none;
 }
