f9925bde9f2e13701c142c15406005e49105dacb
Left7Hand <leoguan24@gmail.com>
2020-11-22 14:16:16 +0800

    qjr

--- vue-library/src/views/SignUp.vue
+++ vue-library/src/views/SignUp.vue
@@ -4,22 +4,23 @@
       <div class="signUpBox">
         <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
         <el-form-item label="注册ID：" prop="id">
-            <el-input v-model.number="ruleForm2.id"></el-input>
+            <el-input v-model.number="ruleForm2.id" onKeypress="javascript:if(event.keyCode == 32)event.returnValue = false;"></el-input>
         </el-form-item>
 
         <el-form-item label="姓名：" prop="name">
-            <el-input v-model="ruleForm2.name"></el-input>
+            <el-input v-model="ruleForm2.name" onKeypress="javascript:if(event.keyCode == 32)event.returnValue = false;"></el-input>
         </el-form-item>
 
-        <el-form-item label="邮箱：" prop="email">
-            <el-input v-model="ruleForm2.email"></el-input>
+        <el-form-item label="邮箱：" prop="email" :rules="[{ required: true, message: '请输入邮箱', trigger: 'blur' },
+      { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }]">
+            <el-input v-model="ruleForm2.email" onKeypress="javascript:if(event.keyCode == 32)event.returnValue = false;"></el-input>
         </el-form-item>
         
         <el-form-item label="密码：" prop="pwd">
-            <el-input type="password" v-model="ruleForm2.pwd" auto-complete="off"></el-input>
+            <el-input type="password" v-model="ruleForm2.pwd" auto-complete="off" onKeypress="javascript:if(event.keyCode == 32)event.returnValue = false;"></el-input>
         </el-form-item>
         <el-form-item label="确认密码：" prop="checkPwd">
-            <el-input type="password" v-model="ruleForm2.checkPwd" auto-complete="off"></el-input>
+            <el-input type="password" v-model="ruleForm2.checkPwd" auto-complete="off" onKeypress="javascript:if(event.keyCode == 32)event.returnValue = false;"></el-input>
         </el-form-item>
         
         <el-form-item>
@@ -57,14 +58,7 @@ import axios from "axios"
             callback();
           }
       };
-      var checkEmail = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('邮箱不能为空'));
-        }
-         else {
-            callback();
-          }
-      };
+      
 
       var validatePass = (rule, value, callback) => {
         if (value === '') {
@@ -101,9 +95,7 @@ import axios from "axios"
           name: [
             { validator: checkName, trigger: 'blur' }
           ],
-          email: [
-            { validator: checkEmail, trigger: 'blur' }
-          ],
+          
           pwd: [
             { validator: validatePass, trigger: 'blur' }
           ],
