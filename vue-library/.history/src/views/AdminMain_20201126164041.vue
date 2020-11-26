94c2d8ba0d1f78958820f3933a8d7867824df53b
JarvanLiang <Jarvanmail@163.com>
2020-11-26 19:53:10 +0800

    Liang

---
+++ vue-library/.history/src/views/AdminMain_20201126164041.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="admin-main-container">
+    <Navigation></Navigation>
+    <h1 class="adminMain-title">欢迎来到管理员界面</h1>
+    <div class="img-container">
+        
+    </div>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Nav.vue'
+export default {
+  components:{
+        Navigation
+    },
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+ 
+
+  },
+};
+</script>
+
+<style scoped>
+.admin-main-container {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(/img/divBg/bg34.jpg);
+  background-size: 100% 100%;
+}
+
+.adminMain-title{
+  margin-top: 0px;
+  text-align: center;
+  font-size: 32px;
+  color: white;
+}
+
+</style>
---
+++ vue-library/.history/src/views/User_20201126162622.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="user-container container">
+        <userNavigation></userNavigation>
+        <h1 class="title">欢迎来到图书管理系统用户界面</h1>
+        <div class="box">
+            <h2 class="sentence">书籍是人类进步的阶梯</h2>
+            <p class="author">————高尔基</p>
+        </div>
+
+
+    </div>
+
+    
+
+</template>
+
+<script>
+import userNavigation from '@/components/userNav.vue'
+export default {
+    components:{
+        userNavigation
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+    created() {
+        
+    },
+}
+</script>
+
+<style scoped>
+.user-container{
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    background-image: url(/img/divBg/bg34.jpg);
+
+}
+
+.user-container .title{
+    float: left;
+    text-align: left;
+    margin-left: 20px;
+    margin-top: 0px;
+    color: white;
+    width: 600px;
+
+}
+
+.box{
+    position: fixed;
+    height: 200px;
+    width: 600px;
+    bottom: 1%; 
+    right: 5%;
+    color: white;
+    
+}
+
+.box h2{
+    font-size: 30px;
+}
+
+.box p{
+    float: right;
+    margin-right: 15px;
+}
+</style>
